06e67754f72759e9f464ac3a1aeea9f5e9aa8721
FelippeTN <felippenalim2004@gmail.com>
2025-08-28 18:36:46 -0300

    STYLE: Novas cores base do tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,9 @@ export default {
 		},
 		extend: {
 			colors: {
+				'dark-blue': '#00034E',
+				'cyan-blue': '#1CBFF0',
+				'light-gray': '#BBC0C1',
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
 				ring: 'hsl(var(--ring))',
